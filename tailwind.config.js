22a2dc17a79ef9b0df0f86023581c3b1a0ddcc38
ryhill86 <ryan.hill09@gmail.com>
2024-02-03 21:29:16 -0700

    IBM Plex Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      md: '640px',
+      lg: '1024px',
+      xl: '1500px', // this is the "design resolution"
+    },
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'Helvetica', ...defaultTheme.fontFamily.sans],
+        sans: ['IBM Plex Sans', 'Helvetica', ...defaultTheme.fontFamily.sans],
       },
     },
   },
